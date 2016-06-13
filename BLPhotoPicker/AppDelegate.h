3951a418613309ed4565adbc60b3549b0973e0c1
Boris <zhouboli@gmail.com>
2016-06-13 10:30:44 +0800

    Initial Commit

---
+++ BLPhotoPicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BLPhotoPicker
+//
+//  Created by Zhouboli on 16/6/13.
+//  Copyright © 2016年 boris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BLPhotoPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLPhotoPicker
+//
+//  Created by Zhouboli on 16/6/13.
+//  Copyright © 2016年 boris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
